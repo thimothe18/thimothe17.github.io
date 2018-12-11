f9c9cfa6b01dd86b8f72784141e5b34a44b8ea3b
Thimothé <das.thimothe@gmail.com>
2018-12-11 21:09:48 +0100

    first update

---
+++ precache-manifest.2a8ea14a972472b96ef8c689f0fad2ee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f40b9218973b5a60c1f",
+    "url": "/static/css/main.52cb49f1.chunk.css"
+  },
+  {
+    "revision": "9f40b9218973b5a60c1f",
+    "url": "/static/js/main.9f40b921.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "932afbdd02713545a16748a8c792cc03",
+    "url": "/index.html"
+  }
+];
